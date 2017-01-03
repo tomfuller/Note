a36fa591046d65cc37634715a936b3d25ce10f39
Teako Warfield-Graham <teakowg@gmail.com>
2017-01-03 15:13:43 +0000

    Creating note model

---
+++ note/note-model.js
@@ -0,0 +1,11 @@
+(function(exports) {
+
+  function Note(text) {
+    this._text = text;
+  };
+
+  Note.prototype.readNote = function() {
+    return this._text;
+  };
+  exports.Note = Note;
+})(this);
